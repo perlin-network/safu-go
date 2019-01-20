40e86c8a9b94de30b25de0a0ae1532ba750d13b3
doug <39219081+doug-perlin@users.noreply.github.com>
2019-01-20 08:54:13 +0800

    api: add get all reports endpoint

--- api/messages.go
+++ api/messages.go
@@ -24,3 +24,19 @@ type QueryAddressResponse struct {
 	TargetAddress string `json:"target_address" validate:"required"`
 	TaintScore    int32  `json:"taint_score" validate:"required"`
 }
+
+type ScamReport struct {
+	ID             string `json:"id"`
+	Timestamp      int64  `json:"timestamp" validate:"required"`
+	AccountID      string `json:"account_id" validate:"required"`
+	ScammerAddress string `json:"scammer_address" validate:"required"`
+	VictimAddress  string `json:"victim_address" validate:"required"`
+	Title          string `json:"title" validate:"required"`
+	Content        string `json:"content" validate:"required"`
+	Proof          string `json:"proof" validate:"required"`
+	Taint          int    `json:"taint"`
+}
+
+type AllScamReportResponse struct {
+	Reports []*ScamReport `json:"reports"`
+}
